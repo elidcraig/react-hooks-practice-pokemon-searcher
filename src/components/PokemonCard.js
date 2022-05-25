d70786dc3c3fc14e1f4c19783a14e9a029124762
Eli Craig <elidcraig@gmail.com>
2022-05-24 20:23:15 -0700

    get request, cards rendering

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({name, hp, sprites}) {
   return (
     <Card>
       <div>
@@ -9,12 +9,12 @@ function PokemonCard() {
           <img alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp} hp
           </span>
         </div>
       </div>
