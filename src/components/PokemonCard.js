d956957ae6beb44d92d2b13d748db3ed5e2c84f1
Eli Craig <elidcraig@gmail.com>
2022-05-24 20:32:11 -0700

    goal #1 complete, images change on click

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({name, hp, sprites}) {
+  const [sprite, setSprite] = useState(true)
+  const handleClick = () => setSprite(!sprite)
+
   return (
     <Card>
-      <div>
+      <div onClick={handleClick}>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={sprite ? sprites.front : sprites.back} alt='Oh no!' />
         </div>
         <div className="content">
           <div className="header">{name}</div>
